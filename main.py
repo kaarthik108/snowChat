8a014c42fb7afb7032d3f1c172b917f2ac41e6d2
kaarthik108 <kaarthikandavar@gmail.com>
2023-04-15 19:54:28 +1200

    Update chat colors

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ st.write("""
 
 
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["WOW :smile:"]
+    st.session_state['generated'] = ["WOW 🤪"]
 if 'past' not in st.session_state:
     st.session_state['past'] = ["Welcome to snowChat! I'm a chatbot designed to help you with Snowflake Database."]
 if "input" not in st.session_state:
@@ -154,11 +154,11 @@ def message(text, is_user=False, key=None, avatar_style="Adventurer"):
     if is_user:
         avatar_url = f"https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue03&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
         message_alignment = "flex-end"
-        message_bg_color = "linear-gradient(135deg, #ff5f6d 0%, #ffc371 100%)"
+        message_bg_color = "linear-gradient(135deg, #00B2FF 0%, #006AFF 100%)"
         avatar_class = "user-avatar"
         st.write(f"""
                 <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                    <div style="background: {message_bg_color}; color: white; border-radius: 5px; padding: 10px; margin-right: 5px; max-width: 75%;">
+                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%;">
                         {text}
                     </div>
                             <img src="{avatar_url}" class="{avatar_class}" alt="avatar" />
@@ -168,12 +168,12 @@ def message(text, is_user=False, key=None, avatar_style="Adventurer"):
     else:
         avatar_url = f"https://avataaars.io/?avatarStyle=Circle&topType=LongHairBun&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
         message_alignment = "flex-start"
-        message_bg_color = "linear-gradient(135deg, #36d1dc 0%, #5b86e5 100%)"
+        message_bg_color = "#71797E"
         avatar_class = "bot-avatar"
         st.write(f"""
                 <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
                     <img src="{avatar_url}" class="{avatar_class}" alt="avatar" />
-                    <div style="background: {message_bg_color}; color: white; border-radius: 5px; padding: 10px; margin-right: 5px; max-width: 75%;">
+                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%;">
                         {text}
                     </div>
                 </div>
